2bf384d632697ce4e1402815a1c0c50cb0f7d5a6
takada-yuto <yuto1999.0516@gmail.com>
2024-07-31 23:35:58 +0900

    ログ確認

--- frontend/app/api/getClientIp/route.ts
+++ frontend/app/api/getClientIp/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
+  console.log(request)
   // クライアントのIPアドレスを取得
   const clientIp = request.headers.get("x-client-ip") || "IP not found"
-  console.log(request)
   console.log(clientIp)
 
   // IPアドレスをJSONでレスポンス
